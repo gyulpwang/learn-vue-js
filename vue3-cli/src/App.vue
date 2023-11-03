f00807a0accddf77f3a27e8736249c59df33b27d
krimseong <krimseong@smilegate.com>
2023-11-03 18:16:16 +0900

    Vue 3 시작하기 강좌 수강

--- vue3-cli/src/App.vue
+++ vue3-cli/src/App.vue
@@ -1,20 +1,13 @@
 <template>
-  <AppHeader 
-    v-bind:appTitle="message"
-    v-on:change="changeMessage">
-  </AppHeader>
+  <AppHeader v-bind:appTitle="message" v-on:change="changeMessage"></AppHeader>
 </template>
 
 <script>
-// import 컴포넌트이름 from './컴포넌트 경로'
+//import 컴포넌트 이름 from './컴포넌트 경로'
 import AppHeader from './components/AppHeader.vue'
 
 export default {
   components: {
-    // '컴포넌트이름': 컴포넌트 내용
-    // 'app-header': AppHeader,
-    // 'AppHeader': AppHeader,
-    // AppHeader: AppHeader,
     AppHeader
   },
   data() {
@@ -23,8 +16,11 @@ export default {
     }
   },
   methods: {
+    showAlert() {
+      alert('hello')
+    },
     changeMessage() {
-      this.message = '변경됨'
+      this.message = '제목 바꿔버렷';
     }
   }
 }
